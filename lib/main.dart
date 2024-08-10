4adba65a9b7a16efed82908689087009c361c023
Ebrahem <ebrahemhashem72@gmail.com>
2024-08-10 10:26:31 +0300

    finish firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:habit_tracker_app/views/sign_in_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HabitTrackerApp());
 }
 
